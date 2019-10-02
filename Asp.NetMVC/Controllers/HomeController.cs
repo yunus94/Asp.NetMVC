7676ad77a73cba31cf2b6c3b96bf8e50c9bde9b7
yunus94 <yunusonal94@gmail.com>
2019-10-03 00:38:03 +0300

    Custom HtmlHelpers Added

--- Asp.NetMVC/Controllers/HomeController.cs
+++ Asp.NetMVC/Controllers/HomeController.cs
@@ -17,6 +17,9 @@ namespace Asp.NetMVC.Controllers
         {
             return View();
         }
-       
+       public ActionResult CustomHtmlHelpers()
+        {
+            return View();
+        }
     }
 }

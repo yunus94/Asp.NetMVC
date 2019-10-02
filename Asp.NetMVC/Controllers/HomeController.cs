0a631d68406ff5e36b92e9e449b17225621152e5
yunus94 <yunusonal94@gmail.com>
2019-10-03 01:09:17 +0300

    UrlLink Added

--- Asp.NetMVC/Controllers/HomeController.cs
+++ Asp.NetMVC/Controllers/HomeController.cs
@@ -21,5 +21,9 @@ namespace Asp.NetMVC.Controllers
         {
             return View();
         }
+        public ActionResult UrlLink()
+        {
+            return View();
+        }
     }
 }

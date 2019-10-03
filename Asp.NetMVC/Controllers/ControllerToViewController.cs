85ef6b08b7a52477c1e19e6828a768838a33e88e
yunus94 <yunusonal94@gmail.com>
2019-10-03 16:35:39 +0300

    Controller To View Added

---
+++ Asp.NetMVC/Controllers/ControllerToViewController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Asp.NetMVC.Controllers
+{
+    public class ControllerToViewController : Controller
+    {
+        // GET: ControllerToView
+        public ActionResult Index()
+        {
+            //ViewData["FullName"] = "yunus önal";     //Viewdata ve viewbag yazım şekli farklı olsada kullanımı aynıdır.
+            //ViewData["check"] = true;
+
+            //TempData["FullName"] = "yunus önal";       tempdata ya farklı sayfalardan da ulaşılabilir. İkinci sayfaya kadar veriyi saklar.
+            //TempData["Check"] = true;
+
+            ViewBag.FullName = "Yunus ÖNAL";            //Viewbag ve viewdata veriyi sadece tek bir sayfada tutar. 
+            ViewBag.Check = true;
+
+            ViewBag.List = new SelectListItem[]
+            {
+                new SelectListItem() {Text = "Malatya"},
+                new SelectListItem() {Text = "Diyarbakır"},
+                new SelectListItem() {Text = "Ankara"},
+                new SelectListItem() {Text = "İstanbul"}
+            };
+            return View();
+        }
+    }
+}

3dd70d5a11dd222fe37f2b0276eaf8af095da16f
yunus94 <yunusonal94@gmail.com>
2019-10-03 14:10:32 +0300

    Section Layout Added

--- Asp.NetMVC/Controllers/ViewsController.cs
+++ Asp.NetMVC/Controllers/ViewsController.cs
@@ -24,5 +24,10 @@ namespace Asp.NetMVC.Controllers
         {
             return View();
         }
+        public ActionResult SectionLayout()
+        {
+            return View();
+        }
+
     }
 }

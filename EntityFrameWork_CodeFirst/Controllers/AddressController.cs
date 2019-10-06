6eb51c629b2f4f6671e0e6d48a8cb2799981eae0
yunus94 <yunusonal94@gmail.com>
2019-10-06 14:48:51 +0300

    DataBase With Insert Operation Added

---
+++ EntityFrameWork_CodeFirst/Controllers/AddressController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EntityFrameWork_CodeFirst.Controllers
+{
+    public class AddressController : Controller
+    {
+        // GET: Address
+        public ActionResult New()
+        {
+            return View();
+        }
+    }
+}
---
+++ EntityFrameWork_CodeFirst/Controllers/PersonController.cs
@@ -0,0 +1,41 @@
+﻿using EntityFrameWork_CodeFirst.Models;
+using EntityFrameWork_CodeFirst.Models.Database_Manager;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EntityFrameWork_CodeFirst.Controllers
+{
+    public class PersonController : Controller
+    {
+        // GET: Person
+        public ActionResult New()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult New(Person person)
+        {
+            DataBaseContext db = new DataBaseContext();
+            db.Persons.Add(person);
+            int sonuc = db.SaveChanges();
+
+            if (sonuc>0)
+            {
+                ViewBag.Result = "Kişi başarılı bir şekilde kaydedilmiştir.";
+                ViewBag.Status = "success";
+            }
+            else
+            {
+                ViewBag.Result = "Kişi kaydedilmedi.";
+                ViewBag.Status = "danger";
+            }
+
+            return View();
+        }
+    }
+}
